02c43025eff7231fdfc6e162af0216aad5aebd5d
jamess-nguyen <haimenguyen@gmail.com>
2025-04-06 11:41:00 -0700

    :memo: added some stylings for lebron clicker

--- src/components/LebronClicker/LebronClicker.jsx
+++ src/components/LebronClicker/LebronClicker.jsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
+import './LebronClicker.css';
 
 const LebronClicker = () => {
     const [clickerCounter, setClickerCounter] = useState(0);
 
     return (
-        <>
+        <div className='clicker-counter'>
             <button>I'm a button</button>
             <p>People who love Lebron: {clickerCounter}</p>
-        </>
+        </div>
     );
 };
 
