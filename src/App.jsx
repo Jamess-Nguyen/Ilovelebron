3aba5c23ccbd485bd22db01301fa08d697195d0b
jamess-nguyen <haimenguyen@gmail.com>
2025-04-06 11:12:23 -0700

    :sparkles: styled headers without images & links

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { ContractEarnings } from './components/ContractEarnings/ContractEarnings
 import { StatEarnings } from './components/StatEarnings/StatEarnings.jsx'
 import { LebronClicker } from './components/LebronClicker/LebronClicker.jsx'
 import { Header } from './components/Header/Header.jsx'
+import './app.css'
 function App() {
   return (
-    <>
+    <div className='app-container'>
       <Header />
       <LebronClicker />
       <ContractEarnings />
       <StatEarnings />
-    </>
+    </div>
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,14 @@
+import './Header.css'
+
 const Header = () => {
     return (
         <>
-            <h1>I Love Lebron James</h1>
+            <h1 className='header-title'>I Love Lebron James</h1>
             <hr></hr>
+            <div className='header-credits'>
+                <p>(Live)</p>
+                <p>Created by Jamer Gamer</p>
+            </div>
         </>
     )
 };
